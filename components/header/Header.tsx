6f59c95771738b96ca2dd7e19b03048bd402bb8b
Ashish aka kratos <ashishcbscboard@gmail.com>
2022-02-03 21:52:09 +0530

    refactor: removed comments

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -3,13 +3,10 @@ import Middle from "./Middle";
 import Right from "./Right";
 function Header() {
   return (
-    <header className="bg-white border-b">
-      <div className="flex justify-between max-w-5xl px-2 lg:px-0 py-3 mx-auto">
-        {/* left section header */}
+    <header className="bg-white border-b sticky top-0 z-20">
+      <div className="flex justify-between max-w-5xl px-2 py-3 mx-auto">
         <Left />
-        {/* middle section header */}
         <Middle />
-        {/* right section header */}
         <Right />
       </div>
     </header>
